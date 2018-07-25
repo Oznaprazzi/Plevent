e0ee9a4c22ffe7f0c2dec07c4e05e1810b8863a3
Dipen Patel <pateldipe1@ecs.vuw.ac.nz>
2018-07-25 16:32:37 +1200

    added some layout began work on the login

---
+++ src/pages/avalibilityplanner/avalibilityplanner.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-avalibilityplanner',
+  templateUrl: 'avalibilityplanner.html'
+})
+export class AvalibilityplannerPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
