62ac03badfbb39939b42373e8ff841dafa98539c
Casey Huang <huangcase@ecs.vuw.ac.nz>
2018-08-01 11:53:10 +1200

    completed loginFunc - starting on Accommodation planner

---
+++ src/pages/addAccommodation/addAccomodation.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-accommodationPlanner',
+  templateUrl: 'addAccomodation.html'
+})
+export class AccommodationPlannerPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
