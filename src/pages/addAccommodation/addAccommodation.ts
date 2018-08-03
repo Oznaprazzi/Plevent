763466bf8433c795aa89e91072dc53cdd58fceb8
Casey Huang <huangcase@ecs.vuw.ac.nz>
2018-08-03 13:01:32 +1200

    added create accommo plan details

--- src/pages/addAccommodation/addAccommodation.ts
+++ src/pages/addAccommodation/addAccommodation.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'page-addAccommodation',
   templateUrl: 'addAccommodation.html'
 })
 export class addAccommodationPage {
-
-  constructor(public navCtrl: NavController) {
+  title: string;
+  street: string;
+  state: string;
+  city: string;
+  country:  string;
+  fromDate: any;
+  toDate: any;
+  price: number;
+  guests: number;
+  error_message = '';
+  constructor(public navCtrl: NavController, public http: HttpClient) {
 
   }
 
+  addAccommo(){
+    this.http.post('http://localhost:8080/accommodation', {
+      title: this.title,
+      street: this.street,
+      state: this.state,
+      city: this.city,
+      country:  this.country,
+      fromDate: this.fromDate,
+      toDate: this.toDate,
+      price: this.price,
+      guests: this.guests
+      },
+      {
+        headers: {'Content-Type': 'application/json'}
+      })
+      .subscribe(res => {
+
+        /*this.error_message = '';
+        this.navCtrl.push(HomePage, {});*/
+
+
+      }, (err) => {
+        this.error_message = "Please fill in all the fields";
+        this.show_error_message = true;
+      });
+  }
 }
