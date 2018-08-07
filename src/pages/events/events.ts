7ea8855d28a2bfe86903c9acb678f5b51df7312a
Dipen Patel <pateldipe1@ecs.vuw.ac.nz>
2018-08-07 22:46:35 +1200

    edit left to do

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -14,9 +14,11 @@ export class EventPage {
 
   userid: number = -1;
   events: any
+
   constructor(public navCtrl: NavController, public http: HttpClient, public navParams: NavParams, public storage: Storage) {
-    
-    storage.get('userid').then((data)=> {
+
+
+    storage.get('userid').then((data) => {
       this.userid = data;
       this.getUser();
       this.getAllEvents();
@@ -36,18 +38,31 @@ export class EventPage {
   getAllEvents() {
 
     this.http.get(`http://localhost:8080/events/event/${this.userid}`).subscribe(res => {
-      this.events  = res as Array<Object>;
+      this.events = res as Array<Object>;
       console.log(this.events);
     }, (err) => {
-      console.log("error"+ err);
+      console.log("error" + err);
     });
   }
 
-  getUser(){
+  getUser() {
     this.http.get(`http://localhost:8080/users/get_user/${this.userid}`).subscribe(res => {
       this.storage.set('userObject', res);
     }, (err) => {
-      console.log("error"+ err);
+      console.log("error" + err);
     });
+  }
+
+  deleteEvent(eventID ) {
+    var id = eventID;
+    console.log(id);
+    this.http.delete(`http://localhost:8080/events/delete_event/${id}`).subscribe(res => {
+      console.log(res);
+      this.getAllEvents();
+    });
+  }
+
+  editEvent() {
+
   }
 }
