6d42aed972056278ff7cd21f5b3a4596b53c7ec1
rabechri <rabechri@ecs.vuw.ac.nz>
2018-08-08 11:39:46 +1200

    created the functionalities for gear list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,12 @@ import { AccommodationsPage } from "../pages/accommodations/accommodations"
 import { AddAccommodationPage } from "../pages/addAccommodation/addAccommodation"
 import { SignupPage } from '../pages/signup/signup';
 import { GroceriesPage } from '../pages/groceries/groceries';
+import { GearsPage } from '../pages/gears/gears';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { IonicStorageModule } from '@ionic/storage';
-import {CreateEventPage} from "../pages/createEvents/createevent";
+import { CreateEventPage } from "../pages/createEvents/createevent";
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import {CreateEventPage} from "../pages/createEvents/createevent";
     AccommodationsPage,
     SignupPage,
     GroceriesPage,
-    CreateEventPage
+    CreateEventPage,
+    GearsPage,
   ],
   imports: [
     BrowserModule,
@@ -47,7 +49,8 @@ import {CreateEventPage} from "../pages/createEvents/createevent";
     AccommodationsPage,
     SignupPage,
     GroceriesPage,
-    CreateEventPage
+    CreateEventPage,
+    GearsPage,
   ],
   providers: [
     StatusBar,
