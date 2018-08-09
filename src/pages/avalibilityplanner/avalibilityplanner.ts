4cf53f3088595c91bd20b4dd4f636b1572507327
Casey Huang <huangcase@ecs.vuw.ac.nz>
2018-08-09 17:26:25 +1200

    started on availability planner

--- src/pages/avalibilityplanner/avalibilityplanner.ts
+++ src/pages/avalibilityplanner/avalibilityplanner.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-avalibilityplanner',
@@ -7,8 +8,35 @@ import { NavController } from 'ionic-angular';
 })
 export class AvalibilityplannerPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController ) {
+    this.showPrompt();
+  }
+  showPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: "Enter a name for this new album you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Enter',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 
