8814605dd893d9d5b89550f7ba7c77fcf8c71b0e
Casey Huang <huangcase@ecs.vuw.ac.nz>
2018-08-09 22:19:38 +1200

    working on edit or delete accommodation

--- src/pages/accommodations/accommodations.ts
+++ src/pages/accommodations/accommodations.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HttpClient} from '@angular/common/http';
 import { AddAccommodationPage } from "../addAccommodation/addAccommodation"
 import { DatePipe } from '@angular/common'
+
+import { Component } from '@angular/core';
+import { ModalController, Platform, NavParams, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-accommodationPlanner',
@@ -10,7 +12,8 @@ import { DatePipe } from '@angular/common'
 })
 export class AccommodationsPage {
   accommodations:any;
-  constructor(public navCtrl: NavController, public http: HttpClient, public datepipe: DatePipe) {
+  constructor(public navCtrl: NavController, public http: HttpClient, public datepipe: DatePipe, public modalCtrl: ModalController) {
+
     this.http.get('http://localhost:8080/accommo/getAccommo')
       .subscribe(res => {
       this.accommodations = res;
@@ -19,10 +22,54 @@ export class AccommodationsPage {
     });
   }
 
+  openModal() {
+
+    let modal = this.modalCtrl.create(ModalContentPage, {"accommo": accommo});
+    modal.present();
+  }
+
   addAccomo(){
     this.navCtrl.push(AddAccommodationPage,{
 
     });
   }
+}
 
+@Component({
+  templateURL: 'editAccommodation.html'
+})
+export class ModalContentPage {
+  accommo: any;
+
+  constructor(public platform: Platform, public params: NavParams, public viewCtrl: ViewController) {
+    accommo = params.get('accommo');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  updateAccommo(){
+    this.http.post(`http://localhost:8080/accommo/edit_accommo/${this.accommo._id}`, {
+        title: this.accommo.title,
+        street: this.accommo.street,
+        state: this.accommo.state,
+        city: this.accommo.city,
+        country:  this.accommo.country,
+        fromDate: this.accommo.fromDate,
+        toDate: this.accommo.toDate,
+        price: this.accommo.price,
+        guests: this.accommo.guests
+      },
+      {
+        headers: {'Content-Type': 'application/json'}
+      })
+      .subscribe(res => {
+        this.error_message = '';
+        this.dismiss();
+      }, (err) => {
+        this.error_message = "Try again ";
+
+      });
+  }
 }
