d6e477a0c3fa1a8a78e48b3372b0449e1f0bc903
Dipen Patel <pateldipe1@ecs.vuw.ac.nz>
2018-08-14 20:10:19 +1200

    back button not refreshing fixed

--- src/pages/avalibilityplanner/avalibilityplanner.ts
+++ src/pages/avalibilityplanner/avalibilityplanner.ts
@@ -24,7 +24,7 @@ export class AvalibilityplannerPage {
 
   }
 
-  ngAfterViewInit() {
+  ionViewWillEnter() {
     this.chart = this.AmCharts.makeChart("chartdiv", {
       "type": "gantt",
       "theme": "dark",
