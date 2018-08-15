993bced3d97cbb1b048e304b63dc1bd258455425
Dipenkumar Patel <pateldipe1@gasoline.ecs.vuw.ac.nz>
2018-08-15 17:45:13 +1200

    git add friend request work WIP

--- src/pages/friend-request/friend-request.ts
+++ src/pages/friend-request/friend-request.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import {HttpClient} from "@angular/common/http";
+import {Storage} from '@ionic/storage';
 
 
 @Component({
@@ -8,9 +9,22 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'friend-request.html',
 })
 export class FriendRequestPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  userObject: any
+  friendRequest: any
+  constructor(public storage: Storage, public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
+    this.storage.get('userObject').then((data) => {
+      this.userObject = data;
+      this.getFriendRequest();
+    });
   }
+
+  getFriendRequest(){
+    this.http.get(`http://localhost:8080/friendsrequest/get_friend_request/${this.userObject._id}`).subscribe(res => {
+      this.friendRequest = res;
+      console.log(res);
+    });
+  }
+
 
 
 
